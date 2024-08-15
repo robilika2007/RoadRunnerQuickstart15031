7039e5af65edcff9f950a51c89ad4df357f8840b
robilika2007 <robert@badea.biz>
2024-08-15 17:49:46 +0300

    bkki

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -99,10 +99,10 @@ public class SampleMecanumDrive extends MecanumDrive {
                 DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
         imu.initialize(parameters);
 
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "FS");
+        leftRear = hardwareMap.get(DcMotorEx.class, "SS");
+        rightRear = hardwareMap.get(DcMotorEx.class, "SD");
+        rightFront = hardwareMap.get(DcMotorEx.class, "FD");
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
